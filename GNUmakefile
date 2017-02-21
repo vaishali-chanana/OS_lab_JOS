d1274680ae7da627afb1dbfe3bd9e0653a8bd880
Nima Honarmand <nhonarmand@cs.stonybrook.edu>
2017-02-21 03:48:22 -0500

    Lab3 initial commit

--- GNUmakefile
+++ GNUmakefile
@@ -141,6 +141,8 @@ $(OBJDIR)/.vars.%: FORCE
 include boot/Makefrag
 # include boot1/Makefrag
 include kern/Makefrag
+include lib/Makefrag
+include user/Makefrag
 
 
 
@@ -230,6 +232,22 @@ handin-check:
 	fi
 
 
+# For test runs
+
+prep-%:
+	$(V)$(MAKE) "INIT_CFLAGS=${INIT_CFLAGS} -DTEST=`case $* in *_*) echo $*;; *) echo user_$*;; esac`" $(IMAGES)
+
+run-%-nox-gdb: prep-% pre-qemu
+	$(QEMU) -nographic $(QEMUOPTS) -S
+
+run-%-gdb: prep-% pre-qemu
+	$(QEMU) $(QEMUOPTS) -S
+
+run-%-nox: prep-% pre-qemu
+	$(QEMU) -nographic $(QEMUOPTS)
+
+run-%: prep-% pre-qemu
+	$(QEMU) $(QEMUOPTS)
 
 # This magic automatically generates makefile dependencies
 # for header files included from C source files we compile,
--- kern/kdebug.h
+++ kern/kdebug.h
@@ -6,15 +6,15 @@
 
 // Debug information about a particular instruction pointer
 struct Ripdebuginfo {
-	const char *rip_file;		// Source code filename for RIP
-	int rip_line;			// Source code linenumber for RIP
+	const char *rip_file;			// Source code filename for RIP
+	int rip_line;					// Source code linenumber for RIP
 
-	const char *rip_fn_name;	// Name of function containing RIP
-					//  - Note: not null terminated!
-	int rip_fn_namelen;		// Length of function name
-	uintptr_t rip_fn_addr;		// Address of start of function
-	int rip_fn_narg;		// Number of function arguments
-	int size_fn_arg[10];		// Sizes of each of function arguments
+	const char *rip_fn_name;		// Name of function containing RIP
+									//  - Note: not null terminated!
+	int rip_fn_namelen;				// Length of function name
+	uintptr_t rip_fn_addr;			// Address of start of function
+	int rip_fn_narg;				// Number of function arguments
+	int size_fn_arg[10];			// Sizes of each of function arguments
 	uintptr_t offset_fn_arg[10];	// Offset of each of function arguments (from CFA)
 	Dwarf_Regtable reg_table;
 };
