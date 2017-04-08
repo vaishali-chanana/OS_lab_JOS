4029ad097c692231e9f521f1002ade8001c06ab7
Nima Honarmand <nhonarmand@cs.stonybrook.edu>
2017-04-08 02:41:04 -0400

    Lab6 initial commit

--- kern/init.c
+++ kern/init.c
@@ -17,6 +17,8 @@
 #include <kern/picirq.h>
 #include <kern/cpu.h>
 #include <kern/spinlock.h>
+#include <kern/time.h>
+#include <kern/pci.h>
 
 uint64_t end_debug;
 
@@ -59,6 +61,10 @@ i386_init(void)
 	// Lab 4 multitasking initialization functions
 	pic_init();
 
+	// Lab 6 hardware initialization functions
+	time_init();
+	pci_init();
+
 	// Acquire the big kernel lock before waking up APs
 	// LAB 4: Your code here.
 
@@ -70,6 +76,11 @@ i386_init(void)
 
 	// Start fs.
 	ENV_CREATE(fs_fs, ENV_TYPE_FS);
+
+#if !defined(TEST_NO_NS) && !defined(VMM_GUEST)
+	// Start ns.
+	ENV_CREATE(net_ns, ENV_TYPE_NS);
+#endif
 
 #if defined(TEST)
 	// Don't touch -- used by grading script!
